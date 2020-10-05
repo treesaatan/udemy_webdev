8024f04568bfa6f05fe3277e8ff0e4426b9cfc91
tes <tressa0923@gmail.com>
2020-10-05 00:24:30 -0700

    add database to yelpcamp and have the new function to add and save new campgrounds to database

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,23 +1,50 @@
 // npm install express ejs --save 
-let express = require("express");
-let app = express();
-let bodyParser = require("body-parser");
+let express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose");
 
+// create yelp_app database with additional items to take care of warnings
+mongoose.connect('mongodb://localhost:27017/yelp_app', { useNewUrlParser: true, useUnifiedTopology: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-let campgrounds = [
-    {name:"Salmon Creek", image:"https://images.unsplash.com/photo-1537905569824-f89f14cceb68?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
-    {name:"Salmon Creek", image:"https://images.unsplash.com/photo-1537905569824-f89f14cceb68?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
-    {name:"Salmon Creek", image:"https://images.unsplash.com/photo-1537905569824-f89f14cceb68?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"},
-]
+// SCHEMA SETUP
+let campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+// turn Schema into a method
+let Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//     {
+//         name:"Alapark", 
+//         image:"https://www.alapark.com/sites/default/files/styles/default/public/2019-04/CCC%20Primitive%20camping.jpeg?itok=_6Gwos6W"
+//     }, function(err, campground){
+//         if(err){
+//             console.log(err);
+//         } else {
+//             console.log("Newly created campground: " + campground);
+//         }
+//     }   
+// );
 
 app.get("/", function(req,res){
     res.render("landing");
 });
 
 app.get("/campgrounds", function(req,res){
-    res.render("campgrounds", {campgrounds:campgrounds});
+    // Get campgrounds from db
+    Campground.find({}, function(err,allCampgrounds){
+        if(err) {
+            console.log(err);
+        } else {
+            res.render("campgrounds", {campgrounds:allCampgrounds});
+        }
+    });
+    // res.render("campgrounds", {campgrounds:campgrounds});
 });
 
 // make new campground
@@ -26,9 +53,15 @@ app.post("/campgrounds", function(req,res){
     let name = req.body.name;
     let image = req.body.image;
     let newCampground = {name:name, image:image};
-    campgrounds.push(newCampground);
-    // redirect to campgrounds
-    res.redirect("/campgrounds");
+    // create new campground and send to the db
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err) {
+            console.log(err);
+        } else {
+            // redirect to campgrounds
+            res.redirect("/campgrounds");
+        }
+    })
 });
 
 app.get("/campgrounds/new", function(req,res){
